72693493ae5e73efd11ce325376974815af97623
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-01 22:00:36 -0600

    Promesa6

--- Ejercicios2.ts
+++ Ejercicios2.ts
@@ -1,14 +1,12 @@
-Promise.reject(new Error('something bad happened'))
+Promise.resolve(123)
+.then((res: any) => {
+throw new Error('something bad happened')
+return 456;
+})
 .then((res: any) => {
 console.log(res); 
-return 456;
+return Promise.resolve(789);
 })
 .catch((err: { message: any; }) => {
 console.log(err.message); 
-return Promise.resolve(123);
 })
-.then((res: any) => {
-console.log(res);
-})
-
-
