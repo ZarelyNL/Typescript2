f1c5fbe11878acff331df0220f8c0cf22a7978a3
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-01 22:28:40 -0600

    Generators7

--- Ejercicios2.ts
+++ Ejercicios2.ts
@@ -1,13 +1,13 @@
 function* generator() {
-    try {
-    yield 'foo';
-    throw Error("Test");
-    }
-    catch(err) {
-    console.log(err.message);
-    }
+        try {
+            yield 'foo';
+            throw Error("Test");
+        }
+        catch(err) {
+            console.log(err.message);
+        }
     }
     var iterator = generator();
     var foo = iterator.next();
     console.log(foo.value);
-    
+    var foo = iterator.next();
